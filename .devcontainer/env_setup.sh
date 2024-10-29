61e266921a0468dd165957617e62a7d3811830db
Alex Schwartz <schwartz_alex@pm.me>
2024-10-29 03:51:33 +0000

    * Cleanup .devcontainer

--- .devcontainer/env_setup.sh
+++ .devcontainer/env_setup.sh
@@ -1,5 +1,30 @@
-#!/bin/bash
+# https://www.reddit.com/r/bash/comments/3gu16u/whats_your_favourite_ps1_prompt/
 
-export PS1='\[\033[01;35m\][bash]\[\033[01;31m\][\u]\[\033[01;34m\]:\W >\[\033[00m\] ';
+# Get Git branch of current directory
+git_branch () {
+    if git rev-parse --git-dir >/dev/null 2>&1
+        then echo -e "" git:\($(git branch 2>/dev/null| sed -n '/^\*/s/^\* //p')\)
+    else
+        echo ""
+    fi
+}
 
-export CLICOLOR=1
+# Set a specific color for the status of the Git repo
+git_color() {
+    local STATUS=`git status 2>&1`
+    if [[ "$STATUS" == *'Not a git repository'* ]]
+        then echo "" # nothing
+    else
+        if [[ "$STATUS" != *'working directory clean'* ]]
+            then echo -e '\033[0;31m' # red if need to commit
+        else
+            if [[ "$STATUS" == *'Your branch is ahead'* ]]
+                then echo -e '\033[0;33m' # yellow if need to push
+            else
+                echo -e '\033[0;32m' # else green
+            fi
+        fi
+    fi
+}
+
+export PS1='\[\033[0;34m\]\u\[\033[0;35m\] at \[\033[0;34m\]\h\[\033[0;35m\] → \[\033[0;30m\]\e[46m[\w]\e[0m$(git_color)$(git_branch)\n\[\033[0;34m\]\$'
