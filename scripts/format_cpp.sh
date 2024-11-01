9980a254ff678f9cecc4c3158c7b974c1812995b
Alex Schwartz <schwartz_alex@pm.me>
2024-11-01 20:52:42 +0000

    * Testing git-clang-format

--- scripts/format_cpp.sh
+++ scripts/format_cpp.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-find ./applications/ -iname "*.cpp" -o -iname "*.h" -o -iname "*.hpp" -o -iname "*.cc" -o -iname "*.hh" | xargs clang-format -i || echo "ran clang-format on applications"
-find ./libraries/ -iname "*.cpp" -o -iname "*.h" -o -iname "*.hpp" -o -iname "*.cc" -o -iname "*.hh" | xargs clang-format -i || echo "ran clang-format on libraries"
+find ./applications/ -iname "*.cpp" -o -iname "*.h" -o -iname "*.hpp" -o -iname "*.cc" -o -iname "*.hh" | xargs git-clang-format
+find ./libraries/ -iname "*.cpp" -o -iname "*.h" -o -iname "*.hpp" -o -iname "*.cc" -o -iname "*.hh" | xargs git-clang-format
